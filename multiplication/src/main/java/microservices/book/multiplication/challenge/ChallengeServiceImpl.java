fdbd3ffeca642dc38286248d26f121f3b116b18f
Matthias <wagenbm@googlemail.com>
2023-01-15 15:59:16 +0100

    Attempt validation including unit test added.

--- multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeServiceImpl.java
+++ multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeServiceImpl.java
@@ -2,11 +2,26 @@ package microservices.book.multiplication.challenge;
 
 import org.springframework.stereotype.Service;
 
+import microservices.book.multiplication.user.User;
+
 @Service
 public class ChallengeServiceImpl implements ChallengeService {
     
     @Override
     public ChallengeAttempt verifyAttempt(ChallengeAttemptDTO attemptDTO) {
-        return null;
+
+        boolean isCorrect = attemptDTO.getGuess() == attemptDTO.getFactorA() * attemptDTO.getFactorB();
+
+        User user = new User(null, attemptDTO.getUserAlias());
+
+        ChallengeAttempt checkedAttempt = new ChallengeAttempt(null, 
+                            user,
+                            attemptDTO.getFactorA(),
+                            attemptDTO.getFactorB(),
+                            attemptDTO.getGuess(),
+                            isCorrect
+        );
+
+        return checkedAttempt;
     }
 }
---
+++ multiplication/src/test/java/microservices/book/multiplication/challenge/ChallengeServiceTest.java
@@ -0,0 +1,38 @@
+package microservices.book.multiplication.challenge;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.BDDAssertions.then;
+
+public class ChallengeServiceTest {
+  
+    private ChallengeService challengeService;
+
+    @BeforeEach
+    public void setup() {
+        challengeService = new ChallengeServiceImpl();
+    }
+
+    @Test
+    public void checkCorrectAttemptTest() {
+        ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "john_doe", 3000);
+
+        ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
+
+        then(resultAttempt.isCorrect()).isTrue();
+
+    }
+
+    @Test
+    public void checkWrongAttemptTest() {
+
+        ChallengeAttemptDTO attemptDTO = new ChallengeAttemptDTO(50, 60, "john_doe", 5000);
+
+        ChallengeAttempt resultAttempt = challengeService.verifyAttempt(attemptDTO);
+
+        then(resultAttempt.isCorrect()).isFalse();
+
+    }
+
+}
