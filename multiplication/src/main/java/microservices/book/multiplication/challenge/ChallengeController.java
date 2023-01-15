6fb26233f65821996d3b84b2a2afcc49a9358f19
Matthias <wagenbm@googlemail.com>
2023-01-15 17:04:23 +0100

    Controller added

---
+++ multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeController.java
@@ -0,0 +1,25 @@
+package microservices.book.multiplication.challenge;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/challenges")
+public class ChallengeController {
+
+    private final ChallengeGeneratorService challengeGeneratorService;
+
+    @GetMapping("/random")
+    Challenge getRandomChallenge() {
+
+        Challenge challenge = challengeGeneratorService.randomChallenge();
+        log.info("Generating a random challenge: {}", challenge);
+
+        return challenge;
+
+    }
+    
+}
