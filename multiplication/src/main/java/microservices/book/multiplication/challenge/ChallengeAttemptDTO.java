d456010b0a9656e8a8e1ff11f10c29457ea68dd1
Matthias <wagenbm@googlemail.com>
2023-02-18 21:54:24 +0100

    Chapter 3 completed

--- multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptDTO.java
+++ multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptDTO.java
@@ -2,10 +2,15 @@ package microservices.book.multiplication.challenge;
 
 import lombok.Value;
 
+import jakarta.validation.constraints.*;
+
 @Value
 public class ChallengeAttemptDTO {
     
+    @Min(1) @Max(99)
     int factorA, factorB;
+    @NotBlank
     String userAlias;
+    @Positive(message = "How could you possibly get a negative result here? Try again.")
     int guess;
 }
