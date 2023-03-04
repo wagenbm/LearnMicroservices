ea02cc77a387658e362b4bd7d56e0a0f5da4babb
Matthias <wagenbm@googlemail.com>
2023-03-04 14:24:44 +0100

    Started chapter 5

--- multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeAttempt.java
+++ multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeAttempt.java
@@ -2,16 +2,21 @@ package microservices.book.multiplication.challenge;
 
 import lombok.*;
 import microservices.book.multiplication.user.User;
+import javax.persistence.*;
 
-/**
+/*
  * Identifies the attempt from a {@link User} to solve a challenge.
  */
-@Getter
-@ToString
-@EqualsAndHashCode
+@Entity
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChallengeAttempt {
+    @Id
+    @GeneratedValue
     private Long id;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
     private User user;
     private int factorA;
     private int factorB;
--- multiplication/src/main/java/microservices/book/multiplication/user/User.java
+++ multiplication/src/main/java/microservices/book/multiplication/user/User.java
@@ -1,16 +1,20 @@
 package microservices.book.multiplication.user;
 
 import lombok.*;
+import javax.persistence.*;
 
 /**
  * Stores information to identify the user.
  */
-@Getter
-@ToString
-@EqualsAndHashCode
+
+@Entity
+@Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
+    @Id
+    @GeneratedValue
     private Long id;
     private String alias;
 
