897adfbd271831bb48a58b355027cdab1cd40646
Matthias <wagenbm@googlemail.com>
2023-03-12 16:36:06 +0100

    Added missing dependency to pom.xml

--- multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptController.java
+++ multiplication/src/main/java/microservices/book/multiplication/challenge/ChallengeAttemptController.java
@@ -1,7 +1,7 @@
 package microservices.book.multiplication.challenge;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+//import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import jakarta.validation.Valid;
 import java.util.List;
 
-@Slf4j
+//@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/attempts")
